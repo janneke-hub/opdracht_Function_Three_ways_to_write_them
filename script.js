580b134ffba3f5206a4f384b426f373a69ec9898
janneke-hub <86882640+janneke-hub@users.noreply.github.com>
2021-10-06 19:34:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function square(number) {
+    console.log(number)
+    return number * number;
+}
+  
+square(5 * 5);
+square(9 * 9);
+
+
+const square1 = function (number) {
+    console.log(number)
+    return number * number;
+};
+  
+square1(5 * 5);
+square1(9 * 9);
+
+
+const square2 = (number) => {
+    console.log(number)
+    return number * number;
+};
+
+square2(5 * 5);
+square2(9 * 9);
+
+
