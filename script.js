6fa70b8066fc9372de98ddc79fabf9d11af66bd5
janneke-hub <86882640+janneke-hub@users.noreply.github.com>
2021-10-06 19:45:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-function square(number) {
-    console.log(number)
-    return number * number;
+function square(number1, number2) {
+    const sum1 = number1 * number1;
+    const sum2 = number2 * number2;
+    const outcome = sum1 + sum2;
+    const outcome = outcome * outcome
+    return outcome;
 }
   
-square(5 * 5);
-square(9 * 9);
+square(5,6);
 
 
-const square1 = function (number) {
-    console.log(number)
-    return number * number;
-};
-  
-square1(5 * 5);
-square1(9 * 9);
-
-
-const square2 = (number) => {
-    console.log(number)
-    return number * number;
+const square1 = function(number1, number2) {
+    const sum3 = number1 * number1;
+    const sum4 = number2 * number2;
+    const outcome1 = sum3 + sum4;
+    const outcome1 = outcome1 * outcome1;
+    return outcome1;
 };
 
-square2(5 * 5);
-square2(9 * 9);
+square1(5,6);
+
+
+const square2 = (number1, number2) => {
+    const sum5 = number1 * number1;
+    const sum6 = number2 * number2;
+    const outcome2 = sum5 + sum6;
+    const outcome2 = outcome2 * outcome2;
+    return outcome2;
+};
+
+square2(5,6);
 
 
